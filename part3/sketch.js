9ed56e9965947b6352edecb6f52cc6a5d597790c
Kyle Erdahl <kyle.m.erdahl@gmail.com>
2019-06-24 14:23:12 -0500

    Refactored code
    Refactored code to not have duplicate methods where unnecessary.
    Also implemented multiple uses of processData() via Promises.all() method.

--- part3/sketch.js
+++ part3/sketch.js
@@ -1,66 +1,78 @@
 const wordnikAPI = `https://api.wordnik.com/v4/words.json/randomword?hasDictionaryDef=true&minDictionaryCount=1&api_key=WORDNIK_API_KEY_HERE}`;
-const giphyAPI = `https://api.giphy.com/v1/gifs/search?api_key=GIPHY_API_KEY_HERE&limit=1&q=`;
-
-// function setup() {
-//   noCanvas();
-
-// //   fetch(wordnikAPI)
-// //     .then(response => response.json())
-// //     .then(json => {
-// //       createP(json.word);
-// //       return fetch(giphyAPI + json.word);
-// //     })
-// //     .then(response => response.json())
-// //       .then(json => createImg(json.data[0].images['fixed_height_small'].url))
-// //       .catch(err => console.log(err));
-// 
-//   processData()
-//     .then(results => {
-//       createP(results.word);
-//       createImg(results.img);
-//     })
-//     .catch(err => console.error(err));
-// }
-
-// async function processData() {
-//   const wordnikResponse = await fetch(wordnikAPI);
-//   const wordnikData = await wordnikResponse.json();
-//   const the_word = wordnikData.word;
-//   const giphyResponse = await fetch(giphyAPI + the_word);
-//   const giphyData = await giphyResponse.json();
-//   const img_url = data.data[0].images['fixed_height_small'].url;
-//   return {
-//     word: the_word,
-//     img: img_url
-//   }
-// }
+const giphyAPI = `https://api.giphy.com/v1/gifs/search?api_key=GIPHY_API_HERE&limit=10&q=`;
 
 function setup() {
   noCanvas();
 
-  // don't have wordnikAPI yet, use default word of "cat"
+  // Using promise format
+
+  // with Wordnik API
+  // fetch(wordnikAPI)
+  //   .then(response => response.json())
+  //   .then(json => {
+  //     createP(json.word);
+  //     return fetch(giphyAPI + json.word);
+  //   })
+  //   .then(response => response.json())
+  //   .then(json => createImg(json.data[0].images['fixed_height_small'].url))
+  //   .catch(err => console.log(err));
+
+  // without Wordnik API
   // createP("cat");
   // fetch(giphyAPI + "cat")
   //   .then(response => response.json())
   //   .then(json => createImg(json.data[0].images['fixed_height_small'].url))
   //   .catch(err => console.log(err));
 
-  processData()
+  // using Promises.all()
+
+  let promises = [processData(), processData(), processData(), processData(), processData()];
+  Promise.all(promises)
     .then(results => {
-      createP(results.word);
-      createImg(results.img);
+      results.forEach(result => {
+        createP(result.word);
+        createImg(result.img);
+      })
     })
     .catch(err => console.error(err));
+
+  // single result using async/await function
+
+  // processData()
+  //   .then(results => {
+  //     createP(results.word);
+  //     createImg(results.img);
+  //   })
+  //   .catch(err => console.error(err));
 }
 
+// with Wordnik API
+// async function processData() {
+//   const wordnikResponse = await fetch(wordnikAPI);
+//   const wordnikData = await wordnikResponse.json();
+//   const the_word = wordnikData.word;
+//   const giphyResponse = await fetch(giphyAPI + the_word);
+//   const giphyData = await giphyResponse.json();
+//   const img_url = data.data[randInt(data.data.length)].images['fixed_height_small'].url;
+//   return {
+//     word: the_word,
+//     img: img_url
+//   }
+// }
+
+// without Wordnik API
 async function processData() {
   const the_word = "cat";
   const response = await fetch(giphyAPI + "cat");
   const data = await response.json();
-  const img_url = data.data[0].images['fixed_height_small'].url;
+  const img_url = data.data[randInt(data.data.length)].images['fixed_height_small'].url;
 
   return {
     word: the_word,
     img: img_url
   }
+}
+
+function randInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
