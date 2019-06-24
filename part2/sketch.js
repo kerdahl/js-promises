f416b41de03d6081cfbe25efd8cd8b18e1c75f32
Kyle Erdahl <kyle.m.erdahl@gmail.com>
2019-06-24 13:29:03 -0500

    Finished Part 2
    Demonstrates how to create, resolve, and reject a Promise.

--- part2/sketch.js
+++ part2/sketch.js
@@ -1,8 +1,20 @@
 function setup() {
-    noCanvas();
-    setTimeout(sayHello, 1000);
+  noCanvas();
+  delay(1000)
+    .then(() => createP("Hello"))
+    .catch(err => console.error(err));
+
+  delay("Not a number")
+    .then(() => createP("Hello"))
+    .catch(err => console.error(err));
 }
 
-function sayHello() {
-    createP("Hello");
+function delay(time) {
+  return new Promise((resolve, reject) => {
+    if (isNaN(time)) {
+      reject(new Error(`"${time}" is not a number.`));
+    }
+
+    setTimeout(resolve, time);
+  });
 }
