3494e0804ad211f7383d935ac4b4e2b9b3a6fc82
Kyle Erdahl <kyle.m.erdahl@gmail.com>
2019-07-29 15:13:44 -0500

    Updates
    Update dependencies and changed part1 to use wordnik api

--- part1/sketch.js
+++ part1/sketch.js
@@ -1,5 +1,5 @@
-//const wordnikAPI = `https://api.wordnik.com/v4/words.json/randomword?hasDictionaryDef=true&minDictionaryCount=1&api_key=WORDNIK_API_KEY_HERE}`;
-const giphyAPI = `https://api.giphy.com/v1/gifs/search?api_key=GIPHY_API_KEY_HERE&limit=1&q=`;
+const wordnikAPI = `https://api.wordnik.com/v4/words.json/randomword?hasDictionaryDef=true&minDictionaryCount=1&api_key=wordnik_api_key`;
+const giphyAPI = `https://api.giphy.com/v1/gifs/search?api_key=giphy_api_key&limit=1&q=`;
 
 // disable ESLint warning; setup method called by p5 library as entry point for javascript
 // eslint-disable-next-line no-unused-vars
@@ -7,20 +7,20 @@ function setup() {
   noCanvas();
 
   // with Wordnik API
-  //   fetch(wordnikAPI)
-  //     .then(response => response.json())
-  //     .then(json => {
-  //       createP(json.word);
-  //       return fetch(giphyAPI + json.word);
-  //     })
-  //     .then(response => response.json())
-  //       .then(json => createImg(json.data[0].images['fixed_height_small'].url))
-  //       .catch(err => console.log(err));
+  fetch(wordnikAPI)
+    .then(response => response.json())
+    .then(json => {
+      createP(json.word);
+      return fetch(giphyAPI + json.word);
+    })
+    .then(response => response.json())
+      .then(json => createImg(json.data[0].images['fixed_height_small'].url))
+      .catch(err => console.log(err));
 
   // without Wordnik API
-  createP("cat");
-  fetch(giphyAPI + "cat")
-    .then(response => response.json())
-    .then(json => createImg(json.data[0].images['fixed_height_small'].url))
-    .catch(err => console.log(err));
+  // createP("cat");
+  // fetch(giphyAPI + "cat")
+  //   .then(response => response.json())
+  //   .then(json => createImg(json.data[0].images['fixed_height_small'].url))
+  //   .catch(err => console.log(err));
 }
